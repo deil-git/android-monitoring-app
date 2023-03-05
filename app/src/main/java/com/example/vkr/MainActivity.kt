3ee41a242286c0fc9c59257c266e447d20c24810
Artemy Bazylev <deil.enot@gmail.com>
2023-03-05 16:16:51 +0300

    -

--- app/src/main/java/com/example/vkr/MainActivity.kt
+++ app/src/main/java/com/example/vkr/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.vkr
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vkr.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +17,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
 
         bindingClass.loginButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Test", Toast.LENGTH_LONG)
-            toast.show()
+            val login = bindingClass.loginText.text
+            val password = bindingClass.passwordText.text
+
+            val client = OkHttpClient()
+            val credential = Credentials.basic(login.toString(), password.toString())
+            val request = Request.Builder()
+                .url("http://web.foodrus.ru/api/tokens")
+                .addHeader("Authorization", credential)
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.d("CREATION", "FAIL")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.d("CREATION", response.body()?.string().toString())
+                }
+            })
+//
+//            val logpass = login.toString().plus(" ").plus(password.toString())
+//            if (login.isNotEmpty() and password.isNotEmpty()) {
+//                val toast = Toast.makeText(this, logpass, Toast.LENGTH_LONG)
+//                toast.show()
+//            }
+
         }
 
 
     }
-
 }
