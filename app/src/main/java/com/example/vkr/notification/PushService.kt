4c1beb832cce5da0bd35f24564a539691f4ba975
Artemy Bazylev <deil.enot@gmail.com>
2023-03-19 18:54:48 +0300

    notification from server

--- app/src/main/java/com/example/vkr/notification/PushService.kt
+++ app/src/main/java/com/example/vkr/notification/PushService.kt
@@ -1,11 +1,32 @@
 package com.example.vkr.notification
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class PushService : FirebaseMessagingService() {
 
     override fun onNewToken(newToken: String) {
         super.onNewToken(newToken)
+    }
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d("Test1", "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d("Test1", "Message data payload: ${remoteMessage.data}")
+
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d("Test1", "Message Notification Body: ${it.body}")
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
 
 }
