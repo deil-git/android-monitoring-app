be861bc74c71a3ef0dde2f379d28796278aa757d
Artemy Bazylev <deil.enot@gmail.com>
2023-03-21 19:55:13 +0300

    two graphs and markers in log

--- app/src/main/java/com/example/vkr/GraphActivity.kt
+++ app/src/main/java/com/example/vkr/GraphActivity.kt
@@ -16,12 +16,12 @@ import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
-import java.util.stream.IntStream.range
 
 
 class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
     lateinit var bindingClass: ActivityGraphBinding
     lateinit var chart: LineChart
+    lateinit var chart2: LineChart
     var atemp = arrayListOf<Float>()
     var ahum = arrayListOf<Float>()
     var atime = arrayListOf<String>()
@@ -33,8 +33,6 @@ class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
         bindingClass = ActivityGraphBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-
-
         chart = findViewById(bindingClass.chart1.id)
         chart.setBackgroundColor(Color.WHITE)
         chart.description.isEnabled = false
@@ -44,15 +42,30 @@ class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
         chart.isDragEnabled = true
         chart.setScaleEnabled(true)
         chart.setPinchZoom(true)
+        chart2 = findViewById(bindingClass.chart1.id)
+        chart2.setBackgroundColor(Color.WHITE)
+        chart2.description.isEnabled = false
+        chart2.setTouchEnabled(true)
+        chart2.setOnChartValueSelectedListener(this)
+        chart2.setDrawGridBackground(false)
+        chart2.isDragEnabled = true
+        chart2.setScaleEnabled(true)
+        chart2.setPinchZoom(true)
 
         val xAxis = chart.xAxis
+        val xAxis2 = chart2.xAxis
         xAxis.enableGridDashedLine(10f,10f,0f)
+        xAxis2.enableGridDashedLine(10f,10f,0f)
 
         val yAxis = chart.axisLeft
-        chart.axisRight.isEnabled = false
+        val yAxis2 = chart2.axisRight
+//        chart.axisRight.isEnabled = false
         yAxis.enableGridDashedLine(10f, 10f, 0f)
-        yAxis.setAxisMaximum(200f)
+        yAxis.setAxisMaximum(40f)
         yAxis.setAxisMinimum(-50f)
+        yAxis2.enableGridDashedLine(10f, 10f, 0f)
+        yAxis2.setAxisMaximum(40f)
+        yAxis2.setAxisMinimum(10f)
 
         val llXAxis = LimitLine(9f, "Index 10")
         llXAxis.lineWidth = 4f
@@ -87,6 +100,9 @@ class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
         chart.animateX(1500)
         val legend = chart.legend
         legend.form = Legend.LegendForm.LINE
+        chart2.animateX(1500)
+        val legend2 = chart2.legend
+        legend2.form = Legend.LegendForm.LINE
 
         Network.getData {
             runOnUiThread(Runnable {
@@ -114,18 +130,16 @@ class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
         for (i in 0 until atemp.size) {
             // turn your data into Entry objects
             values.add(Entry(i.toFloat(), atemp[i]))
-            Log.d("aaa", atemp.toString())
         }
 
-//        val values = ArrayList<Entry>()
-//
-//        for (i in 0 until count) {
-//
-//            val value = (Math.random() * range).toFloat() - 30
-//            values.add(Entry(i.toFloat(), value/*, resources.getDrawable(R.drawable.star)*/))
-//        }
+        val values2: ArrayList<Entry> = ArrayList()
+        for (i in 0 until ahum.size) {
+            // turn your data into Entry objects
+            values2.add(Entry(i.toFloat(), ahum[i]))
+        }
 
         val set1: LineDataSet
+        val set2: LineDataSet
 
         if (chart.data != null && chart.data.dataSetCount > 0) {
             set1 = chart.data.getDataSetByIndex(0) as LineDataSet
@@ -133,36 +147,55 @@ class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
             set1.notifyDataSetChanged()
             chart.data.notifyDataChanged()
             chart.notifyDataSetChanged()
+            set2 = chart2.data.getDataSetByIndex(0) as LineDataSet
+            set2.values = values2
+            set2.notifyDataSetChanged()
+            chart2.data.notifyDataChanged()
+            chart2.notifyDataSetChanged()
         } else {
             // create a dataset and give it a type
             set1 = LineDataSet(values, "DataSet 1")
 
             set1.setDrawIcons(false)
 
+            set2 = LineDataSet(values2, "DataSet 2")
+
+            set2.setDrawIcons(false)
+
             // draw dashed line
             //set1.enableDashedLine(10f, 5f, 0f)
 
             // black lines and points
-            set1.color = Color.BLACK
-            set1.setCircleColor(Color.BLACK)
+            set1.color = Color.GREEN
+            set1.setCircleColor(Color.GREEN)
+
+            set2.color = Color.BLUE
+            set2.setCircleColor(Color.BLUE)
 
             // line thickness and point size
             set1.lineWidth = 1f
             set1.circleRadius = 3f
 
+            set2.lineWidth = 1f
+            set2.circleRadius = 3f
+
             // draw points as solid circles
             set1.setDrawCircleHole(false)
+            set2.setDrawCircleHole(false)
 
             // customize legend entry
             set1.formLineWidth = 1f
+            set2.formLineWidth = 1f
             //set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
             set1.formSize = 15f
+            set2.formSize = 15f
 
             // text size of values
-            set1.valueTextSize = 9f
+            set1.valueTextSize = 0f
+            set2.valueTextSize = 0f
 
             // draw selection line as dashed
-            set1.enableDashedHighlightLine(10f, 5f, 0f)
+            //set1.enableDashedHighlightLine(10f, 5f, 0f)
 
             // set the filled area
 //            set1.setDrawFilled(true)
@@ -175,13 +208,15 @@ class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
 
 
             val dataSets = ArrayList<ILineDataSet>()
-            dataSets.add(set1) // add the data sets
+            dataSets.add(set1)
+            dataSets.add(set2)
 
             // create a data object with the data sets
             val data = LineData(dataSets)
 
             // set data
             chart.data = data
+
         }
     }
 
@@ -190,6 +225,11 @@ class GraphActivity : AppCompatActivity(), OnChartValueSelectedListener {
     }
 
     override fun onValueSelected(e: Entry?, h: Highlight?) {
+        bindingClass.chart1.highlightValue(h);
+        if (h != null) {
+            Log.d("Highlight", "onValueSelected: " + h.getY())
+        };
     }
 
 }
+
